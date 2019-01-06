f8bda17e7a466c4c88097600f9cc294bb4bd8201
Jeff DeLiso <jeff.deliso@gmail.com>
2019-01-05 19:09:47 -0500

    Reorganize files

--- lib/copy.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-require 'fileutils'
-
-directory = File.dirname(__FILE__)
-source = File.join(directory, "..", "..")
-FileUtils.cp_r source, FileUtils.pwd
--- models/application_model.rb
+++
@@ -1,5 +0,0 @@
-require_relative '../lib/sql_object'
-
-class ApplicationModel < SQLObject
-
-end 
